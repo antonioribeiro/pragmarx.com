27fc5cadb4e28123b2746e708a8569a8414669bc
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-11-06 16:17:52 +0000

    Update page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.css" rel="stylesheet" type="text/css">
 
         <style>
             html, body {
@@ -17,6 +18,18 @@
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato';
+            }
+
+            p {
+                line-height: 8px;
+            }
+
+            h2 {
+                margin-bottom: 60px;
+            }
+
+            h1 {
+                margin-top: 60px;
             }
 
             .container {
@@ -38,7 +51,22 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">PragmaRX</div>
+                <h2>Home of Awesome Laravel Packages</h2>
+
+                <h4>
+                    <p><a href="https://github.com/antonioribeiro/google2fa">Laravel Google Two Factor Authentication</a></p>
+                    <p><a href="https://github.com/antonioribeiro/tracker">Laravel Stats Tracker</a></p>
+                    <p><a href="https://github.com/antonioribeiro/firewall">Laravel Firewall</a></p>
+                    <p><a href="https://github.com/antonioribeiro/ci">Continuous Integration Package</a></p>
+                    <p><a href="https://github.com/antonioribeiro/laravelcs">Laravel CS</a></p>
+                    <p><a href="https://github.com/antonioribeiro/support">PragmaRX Support Package</a></p>
+                </h4>
+
+                <h1>
+                    <a href="https://github.com/antonioribeiro"> <i class="fa fa-github-alt" title="Github"></i></a>
+                    <a href="https://twitter.com/iantonioribeiro"> <i class="fa fa-twitter" title="Twitter"></i></a>
+                </h1>
             </div>
         </div>
     </body>
