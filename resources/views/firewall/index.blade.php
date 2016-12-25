fef458005216e00fb90dfa3b0b5760ac9cdf09b4
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-12-25 02:28:09 -0200

    Improve layout

--- resources/views/firewall/index.blade.php
+++ resources/views/firewall/index.blade.php
@@ -9,19 +9,27 @@
     <a href="/firewall/whitelist/{{ $user_ip }}" class="btn btn-success">whitelist</a>
     <a href="/firewall/remove/{{ $user_ip }}" class="btn btn-warning">remove</a>
 
-    @if($whitelist->count())
-        <h2>Whitelisted</h2>
-        @foreach($whitelist as $item)
-            <p class="monospace green"><strong>{{ $item->ip_address }}</strong></p>
-        @endforeach
-    @endif
+    <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-4">
+            @if($whitelist->count())
+                <h2>Whitelisted</h2>
+                @foreach($whitelist as $item)
+                    <p class="monospace green"><strong>{{ $item->ip_address }}</strong></p>
+                @endforeach
+            @endif
+        </div>
+        <div class="col-md-4">
+            @if($blacklist->count())
+                <h2>Blacklisted</h2>
+                @foreach($blacklist as $item)
+                    <p class="monospace red"><strong>{{ $item->ip_address }}</strong></p>
+                @endforeach
+            @endif
+        </div>
+    </div>
 
-    @if($blacklist->count())
-        <h2>Blacklisted</h2>
-        @foreach($blacklist as $item)
-            <p class="monospace red"><strong>{{ $item->ip_address }}</strong></p>
-        @endforeach
-    @endif
 
     <h2>Links</h2>
     <h4>
