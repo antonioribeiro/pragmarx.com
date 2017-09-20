809bcc056e993accb0f2f10a41003a4c6b284360
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-09-20 16:28:55 -0300

    Install ci

---
+++ config/ci.php
@@ -0,0 +1,149 @@
+<?php
+
+return [
+
+	'show_progress' => false,
+
+
+//	'testers' => [
+//		'codeception' => [
+//			'command' => 'sh %project_path%/vendor/bin/codecept run',
+//          'output_folder' => 'tests/_output',
+//          'output_html_fail_extension' => '.fail.html',
+//          'output_png_fail_extension' => '.fail.png'
+//		],
+//
+//      'phpspec' => [
+//	        'command' => 'phpspec run',
+//      ],
+//
+//		'phpunit' => [
+//			'command' => 'phpunit',
+//		],
+//
+//		'behat' => [
+//			'command' => 'sh vendor/bin/behat',
+//		],
+//
+//		'atoum' => [
+//			'command' => 'sh vendor/bin/atoum',
+//		],
+//
+//      'tester' => [
+//	        'command' => 'sh vendor/bin/tester',
+//      ],
+//
+//	],
+
+
+//	'projects' => [
+//		'myproject' => [
+//			'path' => '/var/www/myproject.dev',
+//			'watch_folders' => ['app', 'tests', 'vendor/laravel/framework/tests'],
+//			'exclude_folders' => ['tests/_output'],
+//			'tests_path' => 'tests',
+//			'suites' => [
+//				'functional' => [
+//					'tester' => 'codeception',
+//					'tests_path' => 'functional',
+//					'command_options' => 'functional',
+//					'file_mask' => '*Cept.php',
+//					'retries' => 3,
+//				]
+//			],
+//		],
+
+
+//		'laravel/framework' => [
+//			'path' => '/var/www/myproject.dev/vendor/laravel/framework',
+//			'watch_folders' => ['src', 'tests'],
+//			'exclude_folders' => [
+//				'tests/View/fixtures',
+//				'tests/Support/stubs',
+//			    'tests/Routing/results',
+//			    'tests/Routing/fixtures',
+//			    'tests/Database/stubs/EloquentModelNamespacedStub.php',
+//			],
+//			'tests_path' => 'tests',
+//		    'suites' => [
+//				'unit' => [
+//					'tester' => 'phpunit',
+//					'tests_path' => '',
+//				    'command_options' => '',
+//				    'file_mask' => '*.php',
+//				    'retries' => 3,
+//				]
+//		    ],
+//		],
+
+
+//		'league/flysystem' => [
+//			'path' => '/var/www/myproject.dev/vendor/league/flysystem/',
+//			'watch_folders' => ['src', 'spec'],
+//			'exclude_folders' => [ ],
+//			'tests_path' => 'spec',
+//		    'suites' => [
+//				'spec' => [
+//					'tester' => 'phpspec',
+//					'tests_path' => '',
+//				    'command_options' => '',
+//				    'file_mask' => '*.php',
+//				    'retries' => 0,
+//				]
+//		    ],
+//		],
+
+
+//		'behat/tests' => [
+//			'path' => '/var/www/laravel-ci',
+//			'watch_folders' => ['features'],
+//			'exclude_folders' => [ 'features/bootstrap' ],
+//			'tests_path' => 'features',
+//			'suites' => [
+//				'spec' => [
+//					'tester' => 'behat',
+//					'tests_path' => '',
+//					'command_options' => '',
+//					'file_mask' => '*.feature',
+//					'retries' => 0,
+//				]
+//			],
+//		],
+
+
+//		'atoum/tests' => [
+//			'path' => '/var/www/laravel-ci',
+//			'watch_folders' => ['atoum'],
+//			'exclude_folders' => [ ],
+//			'tests_path' => 'atoum',
+//			'suites' => [
+//				'spec' => [
+//					'tester' => 'atoum',
+//					'tests_path' => '',
+//					'command_options' => '',
+//					'file_mask' => '*.php',
+//					'retries' => 0,
+//				]
+//			],
+//		],
+
+
+//		'nette/tester' => [
+//			'path' => '/var/www/laravel-ci',
+//			'watch_folders' => ['tester'],
+//			'exclude_folders' => [ ],
+//			'tests_path' => 'tester/tests',
+//			'suites' => [
+//				'spec' => [
+//					'tester' => 'tester',
+//					'tests_path' => '',
+//					'command_options' => '',
+//					'file_mask' => '*.php',
+//					'retries' => 0,
+//				]
+//			],
+//		],
+
+//	],
+
+];
