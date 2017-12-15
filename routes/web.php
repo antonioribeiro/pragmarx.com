356b4c22301a8486b83ae78d8d7162ef7a88e4af
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-12-15 14:52:02 -0200

    Remove uneeded code

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,6 @@ Route::get('/carbon', function () {
     return $now->diffInSeconds($before); // returns 0
 });
 
-Route::get('/carbon-real', function () {
-    $before = Carbon\Carbon::now();
-
-    sleep(3);
-
-    Carbon\Carbon::setTestNow(null);
-
-    return Carbon\Carbon::now()->diffInSeconds($before); // returns 0
-});
-
 Route::group(['prefix' => 'api/v1'], function () {
     Route::get('/packages', 'Packages@all');
 });
@@ -33,34 +23,34 @@ Route::group(['prefix' => 'api/v1'], function () {
 require base_path('routes/firewall.php');
 require base_path('routes/google2fa.php');
 
-Route::get('/countries', function () {
-
-//    app(UpdateData::class)->updateAdminStates();
-
-    dd("Countries::where('name.common', 'Brazil')", Countries::where('name.common', 'Brazil'));
-
+//Route::get('/countries', function () {
 //
-//    dump("Countries::where('name.native.por.common', 'Brasil')", Countries::where('name.native.por.common', 'Brasil'));
+////    app(UpdateData::class)->updateAdminStates();
 //
-//    dump("Countries::where('tld.0', '.ch')", Countries::where('tld.0', '.ch'));
-
-//    $state = Countries::where('cca3', 'USA')->first();
+//    dd("Countries::where('name.common', 'Brazil')", Countries::where('name.common', 'Brazil'));
 //
-//    $state = $state->states;
-
-//    dump("Countries::where('cca3', 'USA')->first()->states->pluck('name', 'postal')", dump(Countries::where('cca3', 'USA')->first())->states->pluck('name', 'postal'));
-
-//    dump("Countries::where('cca3', 'FRA')
-//         ->first()
-//         ->borders
-//         ->first()
-//         ->name
-//         ->official", Countries::where('cca3', 'FRA')
-//                               ->first()
-//        ->borders
-//        ->first()
-//        ->name
-//        ->official);
-
-
-});
+////
+////    dump("Countries::where('name.native.por.common', 'Brasil')", Countries::where('name.native.por.common', 'Brasil'));
+////
+////    dump("Countries::where('tld.0', '.ch')", Countries::where('tld.0', '.ch'));
+//
+////    $state = Countries::where('cca3', 'USA')->first();
+////
+////    $state = $state->states;
+//
+////    dump("Countries::where('cca3', 'USA')->first()->states->pluck('name', 'postal')", dump(Countries::where('cca3', 'USA')->first())->states->pluck('name', 'postal'));
+//
+////    dump("Countries::where('cca3', 'FRA')
+////         ->first()
+////         ->borders
+////         ->first()
+////         ->name
+////         ->official", Countries::where('cca3', 'FRA')
+////                               ->first()
+////        ->borders
+////        ->first()
+////        ->name
+////        ->official);
+//
+//
+//});
